c573437c7a1ad9ba0b5be6d97d2fcb1f6ab7e424
Khoa Tran <dkhoa.tran@gmail.com>
2023-04-14 18:21:56 +0700

    Python 3.9 compatibility

--- zalo_sdk/BaseClient.py
+++ zalo_sdk/BaseClient.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+
 class BaseClient:
     def __init__(self, access_token: str = "", refresh_token: str = ""):
         self._refresh_token = refresh_token
@@ -23,24 +24,23 @@ class BaseClient:
         self._access_token = token
 
     def send_request(self, method: str, url: str, body: object | str) -> requests.Response:
-        match method:
-            case "POST":
-                headers = {
-                    'Content-Type': "application/json",
-                    'access_token': self._access_token
-                }
-                response = requests.post(
-                    url,
-                    json=body,
-                    headers=headers)
-            case "GET":
-                headers = {
-                    'access_token': self._access_token
-                }
-                response = requests.get(
-                    url,
-                    params={"data": json.dumps(body)},
-                    headers=headers)
-            case _:
-                raise Exception(f"Unknown method '{method}'")
+        if method == "POST":
+            headers = {
+                'Content-Type': "application/json",
+                'access_token': self._access_token
+            }
+            response = requests.post(
+                url,
+                json=body,
+                headers=headers)
+        elif method == "GET":
+            headers = {
+                'access_token': self._access_token
+            }
+            response = requests.get(
+                url,
+                params={"data": json.dumps(body)},
+                headers=headers)
+        else:
+            raise Exception(f"Unknown method '{method}'")
         return response
