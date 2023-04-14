4f6dd611036a0dfdf570c77621b5662ae41086d1
Khoa Tran <dkhoa.tran@gmail.com>
2023-04-14 16:35:57 +0700

    Typo

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ with open('LICENSE') as f:
 setup(
     name='zalo_sdk',
     version='0.1.0',
-    description='Zalo SDK'
+    description='Zalo SDK',
     long_description=readme,
     author='Khoa Tran',
-    author_email='khoa@connext.biz'
+    author_email='khoa@connext.biz',
     url='https://github.com/connext_biz',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
