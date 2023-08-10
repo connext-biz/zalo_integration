5459d22ce86716ed9fabbb04dc8caaa3943bea6e
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2023-08-10 14:36:06 +0700

    fix: Upload test cases for testing create_request_header function

--- run_tests.py
+++ run_tests.py
@@ -1,23 +1,11 @@
 import unittest
-from test.test_send_text_message import ZaloSendTextMessage
-from test.test_send_reply_message import ZaloSendReplyMessage
-from test.test_send_media_message import ZaloSendMediaMessage
-from test.test_send_request_form_message import ZaloSendRequestFormMessage
-from test.test_send_sticker_message import ZaloSendStickerMessage
-from test.test_send_transaction_message import ZaloSendTransactionMessage
-from test.test_send_file_message import ZaloSendFileMessage
-from test.test_get_oa_information import ZaloGetOAInfoMessage
+from test.test_oa_create_request_body import ZaloSendRequestBody
+from test.test_create_request_header import ZaloSendRequestHeader
 
 def create_test_suite():
     test_suite = unittest.TestSuite()
-    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ZaloSendTextMessage))
-    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ZaloSendReplyMessage))
-    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ZaloSendMediaMessage))
-    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ZaloSendRequestFormMessage))
-    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ZaloSendStickerMessage))
-    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ZaloSendTransactionMessage))
-    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ZaloSendFileMessage))
-    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ZaloGetOAInfoMessage))
+    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ZaloSendRequestBody))
+    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ZaloSendRequestHeader))
 
     return test_suite
 
---
+++ test/test_create_request_header.py
@@ -0,0 +1,42 @@
+from zalo_sdk.BaseClient import BaseClient
+from zalo_sdk.oa.ZaloMessage import *
+
+from . import ZaloSendMessage
+
+
+class ZaloSendRequestHeader(ZaloSendMessage):
+
+    def test_create_request_header(self):
+        client = BaseClient(
+            app_id=self.app_id, 
+            secret_key=self.secret_key, 
+            access_token=self.access_token, 
+            refresh_token=self.refresh_token
+        )
+
+        header = client.create_request_header(method="POST")
+
+        data = {
+            'Content-Type': "application/json",
+            'access_token': self.access_token,
+        }
+
+        self.assertEqual(header, data)
+
+    def test_get_oa_information_header(self):
+        client = BaseClient(
+            app_id=self.app_id, 
+            secret_key=self.secret_key, 
+            access_token=self.access_token, 
+            refresh_token=self.refresh_token
+        )
+
+        
+        # Call the create_request_header function and assert the response
+        header = client.create_request_header(method="GET")
+
+        data = {
+            'access_token': self.access_token,
+        }
+
+        self.assertEqual(header, data)
