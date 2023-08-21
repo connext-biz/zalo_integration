0c77d7faa2a0c9252fff41d6107857a6254d33bf
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2023-08-21 14:40:06 +0700

    fix: update zalo_sdk version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE') as f:
 
 setup(
     name='zalo_sdk',
-    version='0.2.3',
+    version='0.2.4',
     description='Zalo SDK',
     long_description=readme,
     author='Khoa Tran',
