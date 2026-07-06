85a901f6023211c3e7a1d7d179cf19f4bb4ab5ae
Dang Nguyen <138419414+minhdanghorus@users.noreply.github.com>
2026-07-06 21:56:11 +0700

    feat: add Zalo OA tag methods (#19)
    Co-authored-by: dangntm <dangntm>

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,7 @@ from test.test_oa_create_request_body import ZaloSendRequestBody
 from test.test_create_request_header import ZaloSendRequestHeader
 from test.test_get_user_detail import ZaloGetUserDetail
 from test.test_get_zalo_quotas import ZaloGetZaloQuotas
+from test.test_oa_tag_methods import ZaloOATagMethods
 
 def create_test_suite():
     test_suite = unittest.TestSuite()
@@ -10,6 +11,7 @@ def create_test_suite():
     test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ZaloSendRequestHeader))
     test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ZaloGetUserDetail))
     test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ZaloGetZaloQuotas))
+    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(ZaloOATagMethods))
 
     return test_suite
 
